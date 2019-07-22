eeb066218459d18641496a8ebb7ec1413e67aa9a
Bykov Slava <avel2k6@gmail.com>
2019-07-22 20:08:36 +0300

    add routes.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,22 @@ import '../assets/application.css';
 // import io from 'socket.io-client';
 
 import initApp from './app.jsx';
-
+import axios from 'axios';
+import routes from './routes.js';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
 const { gon } = window;
+console.log(routes.channelsPath());
+axios.get(routes.channelsPath())
+ .then(
+    (res) => {
+      console.log(res);
+    }
+  )
+  .catch(
+    e => console.log(e)
+  );
 initApp(gon);
