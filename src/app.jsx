fff29debdf7e213d5038bc35dc68bf18a2fe5642
Bykov Slava <avel2k6@gmail.com>
2019-07-31 10:32:06 +0300

    moved getUserName to index.js and refactor this function

--- src/app.jsx
+++ src/app.jsx
@@ -4,8 +4,6 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
-import cookies from 'js-cookie';
-import { internet } from 'faker';
 import reducers from './reducers';
 import App from './components/App';
 import UsernameContext from './components/UsernameContext';
@@ -30,26 +28,14 @@ const store = createStore(
   composeMiddleware,
 );
 
-const getUserName = () => {
-  const getName = () => cookies.get('userName');
-  const setName = (name) => {
-    cookies.set('userName', name, { expires: 360 });
-    return name;
-  };
 
-  return getName()
-    ? getName()
-    : setName(internet.userName());
-};
-
-export default (gon) => {
+export default (gon, userName) => {
   store.dispatch(initChat(gon));
   listenNewMessage(store.dispatch);
   listenNewChannel(store.dispatch);
   listenRenameChannel(store.dispatch);
   listenDeleteChannel(store.dispatch);
 
-  const userName = getUserName();
   render(
     <Provider store={store}>
       <UsernameContext.Provider value={userName}>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/application.css';
+import { internet } from 'faker';
+import cookies from 'js-cookie';
 import initApp from './app.jsx';
-// import gon from 'gon';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
+
+const getUserName = () => {
+  if (cookies.get('userName')) {
+    return cookies.get('userName');
+  }
+  const userName = internet.userName();
+  cookies.set('userName', userName, { expires: 360 });
+  return userName;
+};
+
+const userName = getUserName();
+
 const { gon } = window;
-initApp(gon);
+initApp(gon, userName);
